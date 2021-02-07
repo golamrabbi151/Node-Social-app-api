8fb8e2f36d5fa12cccb4dbbd8613be4bef380aec
golamrabbi151 <rabbi15-242@diu.edu.bd>
2021-02-07 15:24:05 +0600

    bla bla bla

--- api/controller/UserController.js
+++ api/controller/UserController.js
@@ -1,32 +1,55 @@
 const User = require("../../model/User")
+const bcrypt = require("bcrypt")
+const saltRounds = 10
 
 
-const Create = async (req,res,next) => {
-try{
-    const {
-        name,
-        email,
-        // phone,
-        about,
-        role
-    }=req.body
+const RegisterUser = async (req, res, next) => {
+    try {
+        const {
+            name,
+            email,
+            password,
+            about
+            // role
+        } = req.body
 
-    const checkEmail = await User.findOne({email:email})
-    if(checkEmail) return res.status(409).json({status:false,message:"Email alredy Exist"})
+        const checkEmail = await User.findOne({ email: email })
+        if (checkEmail) return res.status(409).json({ status: false, message: "User alredy Exist" })
+        // encrypt password
+        const upass = bcrypt.hashSync(password, saltRounds)
 
-    const saveUser = new User({
-        name:name,
-        email:email,
-        about:about,
-        role:role
-    })
-    await saveUser.save();
-    return res.status(201).json({status:true, message:"User Create Successful"})
+        const saveUser = new User({
+            name: name,
+            email: email,
+            password: upass,
+            about: about
+            // role:role
+        })
+        await saveUser.save();
+        return res.status(201).json({ status: true, message: "User Create Successful" })
+    }
+    catch (error) {
+        res.json({ message: error.message })
+    }
 }
-catch(error){
-    if(error) console.log(error)
-    next(error)
-}
+
+const LoginUser = async (req, res, next) => {
+    try {
+        const { email, password } = req.body
+        // encrypt pass
+       
+    //    Login User
+       const login = await User.findOne({email:email})
+       if(!login) return res.status(404).json({message:"Email Not found"})
+        // compare password
+       const result = await bcrypt.compareSync(password,login.password)
+       if(!result) return res.json({message: "password Not match"})
+
+       return res.json({login})
+           }
+    catch (error) {
+        res.json({message:error.message})
+    }
 }
 
-module.exports={Create}
+module.exports = { RegisterUser,LoginUser}
--- app.js
+++ app.js
@@ -2,17 +2,30 @@ const express = require("express")
 const morgan = require("morgan")
 const bodyParser = require("body-parser")
 const mongoose = require("mongoose")
-const app =express();
+const app = express();
 
-app.use(morgan('tiny'))
-app.use(bodyParser.urlencoded({extended:true}))
+app.use(morgan('dev'))
+app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json());
 
 const routePath = require("./api/routes/user")
-app.use("/api/v0",routePath)
+app.use("/api/v0", routePath)
+
+app.use((req, res, next) => {
+  let error = new Error("404 page not found")
+  error.status = 404
+  next(error);
+})
+
+app.use((res, error) => {
+  if (error === 404) return res.status(404).json({ message: error.message })
+  if (error === 400) return res.status(400).json({ message:  error.message })
+  return res.status(500).json({ message:  error.message })
+})
 
 
- mongoose.connect('mongodb://localhost:27017/social', {
+
+mongoose.connect('mongodb://localhost:27017/social', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useFindAndModify: false,
@@ -20,10 +33,10 @@ app.use("/api/v0",routePath)
 });
 
 const db = mongoose.connection
-db.on('error',(err)=>console.log(err.message))
-db.once("open",()=>console.log("DB Connection Successful"))
+db.on('error', (err) => console.log(err.message))
+db.once("open", () => console.log("DB Connection Successful"))
 const port = process.env.port || 3000;
 
-app.listen(port,()=>{
-    console.log("app is running")
+app.listen(port, () => {
+  console.log("app is running")
 })
